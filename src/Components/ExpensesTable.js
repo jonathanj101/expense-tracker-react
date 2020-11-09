736248bcc19670781735bec88a3611d43365c3be
jonathanj101 <jonatjimen@gmail.com>
2020-11-09 17:48:11 -0500

    looking for a way to populate rows properly
    looking for a way to populate rows properly

--- src/Components/ExpensesTable.js
+++ src/Components/ExpensesTable.js
@@ -2,38 +2,29 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 
 const ExpensesTable = props => {
-    console.log(props)
-    const isSubmiited = props.state.submitted
-
-
-
     return (
         <Table className='main-table' striped bordered hover >
             <thead>
                 <tr>
-                    <th>#</th>
                     <th>Description</th>
                     <th>Amount</th>
                     <th>Payment Type</th>
                     <th>Date</th>
-                    <th>Delete</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr >
-                    <th>1</th>
                     <th>Domino's Pizza</th>
                     <th>$35</th>
                     <th>Credit Card</th>
                     <th>10-20-2020</th>
-                    <th className="close">&times;</th>
                 </tr>
-                {isSubmiited ? props.state.rows.map(row => <tr><th>{row}</th></tr>) : console.log('nope')}
+                {/* {isSubmiited ? props.state.rows.map(row => <th>{row}</th>) : console.log('nope')} */}
+                {/* {isSubmiited ? props.state.rows.map(row => <tr><th>{row}</th></tr>) : console.log('ok', props.state.rows)} */}
             </tbody>
         </Table >
     )
-
 }
 
 export default ExpensesTable
