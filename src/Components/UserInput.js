d449384af0211c68cd7dd5299362d19e79f064fb
jonathanj101 <jonatjimen@gmail.com>
2020-11-09 00:12:11 -0500

    grabbing input values and textContent porperly, still need work
    grabbing input values and textContent porperly, still need work

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,86 +1,54 @@
 import React from 'react'
-import { InputGroup, FormControl } from 'react-bootstrap'
-
-
-
 
 
 const UserInput = props => {
+    const testing = () => {
+        const description = document.getElementById('description').textContent
+        const amount = document.getElementById('amount').textContent
+        const date = document.getElementById('date').value
+        const payment = document.getElementById('dropdownMenu2').textContent
+
+        if (description === null || amount === null || date === null || payment === 'Payment Method') {
+            alert("Empty Expense detail ==>> no Expense to track!!")
+        } else {
+            props.onSubmit()
+        }
+    }
+    const handleChange = (e) => {
+        let paymentTitle = document.getElementById('dropdownMenu2')
+        paymentTitle.textContent = e.target.outerText
+        console.log(e.target.textContent)
+    }
+
     return (
-        <div className="w-100">
-            <p className="alert alert-primary" role="alert">Expenses</p>
-            <div className="w-50">
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Desscription"
-                        aria-label="Description"
-                        ref={props.state.description}
-
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Amount"
-                        aria-label="Amount"
-                        ref={props.state.amount}
-                    />
-                </InputGroup>
-                <div className="dropdownt">
-                    <button
-                        className="btn btn-primary btn-block btn-lg dropdown-toggle mb-3" type="button"
-                        id="dropdownMenu2"
-                        data-toggle="dropdown"
-                        aria-haspopup="true">
-                        {props.state.paymentMethodTitle}
-                    </button>
-                    <div className="dropdown-menu " aria-labelledby="dropdownMenu2">
-                        <button
-                            onClick={props.handleChange}
-                            value="Cash"
-                            className="dropdown-item"
-                            type="button">
-                            Cash
-                        </button>
-                        <button
-                            onClick={props.handleChange}
-                            value="Debit Card"
-                            className="dropdown-item"
-                            type="button">
-                            Debit Card
-                        </button>
-                        <button
-                            onClick={props.handleChange}
-                            value="Credit Card"
-                            className="dropdown-item"
-                            type="button">
-                            Credit Card
-                        </button>
-                    </div>
+        <form className='w-100'>
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                    <input type="text" className="form-control" id="description" placeholder="Restaurant, Grocery, Bills etc..." />
                 </div>
-
-                {/* <DropdownButton className="btn mb-3" onSelect={this.handleChange} id="dropdown-basic-button" title={title}>
-                    <Dropdown.Item as="button" eventKey="Cash">Cash</Dropdown.Item>
-                    <Dropdown.Item as="button" eventKey="Debit Card">Debit Card</Dropdown.Item>
-                    <Dropdown.Item as="button" eventKey="Credit Card">Credit Card</Dropdown.Item>
-                </DropdownButton> */}
-                <InputGroup >
-                    <FormControl
-                        placeholder='Date of Expense'
-                        aria-label='Date of Expense'
-                        type='date'
-                        ref={props.state.date}
-                    />
-                </InputGroup>
-                <div className="container">
-                    <button
-                        onClick={props.onSubmit}
-                        type="submit"
-                        className="btn btn-outline-success btn-block">
-                        Add Expense
-                    </button>
+                <div className="form-group col-md-6">
+                    <input type="text" className="form-control" id="amount" placeholder="Amount" />
                 </div>
             </div>
-        </div >
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                    <input type="date" className="form-control" id="date" placeholder="Date of Expense" />
+                </div>
+                <div className="form-group col-md-6">
+                    <div className="dropdown">
+                        <button onClick={handleChange} className="btn btn-primary btn-block dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Payment Method
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <button onClick={handleChange} className="dropdown-item" type="button">Cash</button>
+                            <button onClick={handleChange} className="dropdown-item" type="button">Debit Card</button>
+                            <button onClick={handleChange} className="dropdown-item" type="button">Credit Card</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button onClick={testing} type="button" className="btn btn-primary btn-block">Add Expense</button>
+        </form >
     )
 }
 export default UserInput
