1776f6300867c3fa6a7f85320174e83e28ee9065
jonathanj101 <jonatjimen@gmail.com>
2020-11-09 22:25:38 -0500

    table show parent state data as rows properly
    table show parent state data as rows properly

--- src/Components/ExpensesTable.js
+++ src/Components/ExpensesTable.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
 
-const ExpensesTable = props => {
+const ExpensesTable = (props) => {
+    const expenses = props.state.rows;
+    console.log(expenses)
+    const expense = expenses.map((expense, num) => {
+        return (
+            <tr>
+                <th>{num}</th>
+                <th>{expense[0]}</th>
+                <th>{expense[1]}</th>
+                <th>{expense[2]}</th>
+                <th>{expense[3]}</th>
+            </tr>
+        )
+    })
+    console.log(expense)
+    // const isSubmitted = props.state.submitted
+    // console.log(typeof (props.state.rows))
     return (
         <Table className='main-table' striped bordered hover >
             <thead>
                 <tr>
+                    <th>#</th>
                     <th>Description</th>
                     <th>Amount</th>
                     <th>Payment Type</th>
@@ -14,17 +31,11 @@ const ExpensesTable = props => {
             </thead>
 
             <tbody>
-                <tr >
-                    <th>Domino's Pizza</th>
-                    <th>$35</th>
-                    <th>Credit Card</th>
-                    <th>10-20-2020</th>
-                </tr>
-                {/* {isSubmiited ? props.state.rows.map(row => <th>{row}</th>) : console.log('nope')} */}
-                {/* {isSubmiited ? props.state.rows.map(row => <tr><th>{row}</th></tr>) : console.log('ok', props.state.rows)} */}
+                {expense}
             </tbody>
         </Table >
     )
 }
 
 export default ExpensesTable
+// {isSubmitted ? console.log(expense) : console.log(expense)}                 {/* {isSubmitted ? props.state.rows.map(row => <tr><th>{row}</th></tr>) : console.log('nope')} */}                 {/* {isSubmiited ? props.state.rows.map(row => <tr><th>{row}</th></tr>) : console.log('ok', props.state.rows)} */}
